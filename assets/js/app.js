3a886982e6cd7ecfa89e1275b8d316d5fc605838
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2017-07-14 23:12:34 -0300

    Initial push

---
+++ assets/js/app.js
@@ -0,0 +1,3 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); 
+particlesJS.load('content', 'https://raw.githubusercontent.com/sandrofolk/conttudoweb/master/assets/js/particles.json', function() {console.log('callback - particles.js config loaded');});
+*/
